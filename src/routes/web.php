d5297c5a7e7d533d6fa1c3e0a4b1f2eb3feec849
drimcaster <drimcaster@gmail.com>
2024-09-01 14:41:05 +0800

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
 use iProtek\SmsSender\Http\Controllers\SmsController;
 
-Route::prefix('sms-sender')->name('sms-sender')->group(function(){
-    Route::get('/', [SmsController::class, 'index'])->name('.index');
+Route::middleware(['web'])->group(function(){
+ 
+    Route::middleware(['auth'])->prefix('manage/sms-sender')->name('manage.sys-notification')->group(function(){
+        //Route::get('/test', [SysNotificationController::class, 'index'])->name('.test');
+        //Route::get('/', [SysNotificationController::class, 'index'])->name('.index');
+        //Route::get('/system-updates', [SysNotificationController::class, 'system_updates'])->name('.system-updates');
+        //oute::post('/check-system-updates', [SysNotificationController::class, 'check_system_updates'])->name('.check-system-updates');
+        //Route::get('/check-system-updates', [SysNotificationController::class, 'check_system_updates'])->name('.get-check-system-updates');
+        //Route::get('/system-updates-summary', [SysNotificationController::class, 'system_updates_summary'])->name('.check-system-summary');
+        //Route::post('/apply-system-updates', [SysNotificationController::class, 'apply_system_updates'])->name('.apply-system-updates');
+     
+        Route::get('/', [SmsController::class, 'index'])->name('.index'); 
+
+    });
+  
 });
