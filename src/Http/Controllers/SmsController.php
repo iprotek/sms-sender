7754497a2839f7d7c86941bfb8f36ff3deb5eb51
drimcaster <drimcaster@gmail.com>
2024-09-03 12:22:36 +0800

    Update SmsController.php

--- src/Http/Controllers/SmsController.php
+++ src/Http/Controllers/SmsController.php
@@ -13,10 +13,25 @@ class SmsController extends Controller
     }
 
     public function push_notif_info(Request $request){
+        $base = config('iprotek.pay_message_url');
 
-        $cli = \iProtek\Core\Helpers\PayHttp::client_info(); 
-        if($cli)
-            return $cli['socket_settings'];
+        if( !$base || !trim($base)){
+
+            return [
+                "is_active"=>null,
+                "name"=>"",
+                "key"=>"",
+                "cluster"=>""
+            ];
+
+        }
+        
+        $url = $base."/api/push-info";
+        $cli = \iProtek\Core\Helpers\PayHttp::get_client_load($url); 
+        if($cli){
+            return $cli;
+            //return $cli['socket_settings'];
+        }
         return [
             "is_active"=>false,
             "name"=>"",
