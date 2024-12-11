953a7c2b59bd77c7029e442476b72548e08a43bf
drimcaster <drimcaster@gmail.com>
2024-12-12 05:10:49 +0800

    Update guest-chat.blade.php

--- resources/views/mail/chat/guest-chat.blade.php
+++ resources/views/mail/chat/guest-chat.blade.php
@@ -1,4 +1,4 @@
-Hi {{ $person_name }},
-<p style="text-indent:10px;"> This is to verify code: {{ $code }} request from {{ $url }} </p>
+Hi <b>{{ $person_name }}</b>,
+<p style="text-indent:10px;"> This is the verify code: <b>{{ $code }}</b> request from {{ $url }} </p>
 
 Thank you.
