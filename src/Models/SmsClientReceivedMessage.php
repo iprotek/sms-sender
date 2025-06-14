d23beee24a07e107519dc3d9726995add824140c
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-06-14 15:43:31 +0800

    Update SmsClientReceivedMessage.php

--- src/Models/SmsClientReceivedMessage.php
+++ src/Models/SmsClientReceivedMessage.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Log;
+use iProtek\SmsSender\Helpers\PaySmsHelper;
 
 class SmsClientReceivedMessage extends Model
 {
