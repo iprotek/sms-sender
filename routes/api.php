63679d1a94ce0974b754f1bb1d6ad7afe214d91c
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-04-30 21:47:11 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::prefix('api')->middleware('api')->name('api')->group(function(){
 
     Route::prefix('message')->name('.message')->group(function(){
 
-      Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){ 
+      Route::prefix('group/{group_id}')->middleware(['pay.api', 'policy.control'])->name('api')->group(function(){ 
           //FILE UPLOADS
           //include(__DIR__.'/api/file-upload.php');
 
